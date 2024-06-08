30297398ed0d0f240600fd15989309ae269f8f9b
MiskirB <156695703+MiskirB@users.noreply.github.com>
2024-06-09 00:22:49 +0300

    pages2

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
 	//local instance of firebase function
 	// deployed version of firebase functions
-	baseURL: "http://127.0.0.1:5001/clone-mb/us-central1/api",
+	// baseURL: "http://127.0.0.1:5001/clone-mb/us-central1/api",
 	
-	// baseURL:"https://amazon-api-deploy-xvzi.onrender.com/",
+	baseURL:"https://amazon-api-deploy-xvzi.onrender.com/",
 // deployed version of amzazon server on render.com
 	
 });
